07be096c59dbf812de265a25465fdca1bb00bf63
javierxyz <jrojasxyz@gmail.com>
2022-11-04 19:18:18 -0400

    Actualizaciones

---
+++ src/app/agregarActualizacion/agregarActualizacion.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-agregarActualizacion',
+  templateUrl: './agregarActualizacion.component.html',
+  styleUrls: ['./agregarActualizacion.component.css']
+})
+export class AgregarActualizacionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/itinerario-aviones/itinerario-aviones.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-itinerario-aviones',
+  templateUrl: './itinerario-aviones.component.html',
+  styleUrls: ['./itinerario-aviones.component.css']
+})
+export class ItinerarioAvionesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

735708d19ce4ae8e6287742da1defb1e8e82a9eb
thecarrot912 <thecarrot911@gmail.com>
2023-04-12 00:17:57 -0400

    Creando el modal actualizacion

--- src/app/agregarActualizacion/agregarActualizacion.component.ts
+++ src/app/agregarActualizacion/agregarActualizacion.component.ts
@@ -1,5 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Type, Input, EventEmitter, Output } from '@angular/core';
+import { Empleado } from '../empleados';
+import { AllempleadosService } from '../services/allempleados.service';
 import { HorarioService } from '../services/horario.service';
+import { DatePipe } from '@angular/common';
+
 
 @Component({
     selector: 'app-agregarActualizacion',
@@ -10,8 +14,21 @@ import { HorarioService } from '../services/horario.service';
 
 export class AgregarActualizacionComponent implements OnInit {
 
+
+    // Variable que recibe de schedule 
+    @Input() planificacion_id: number;
+    
+    // Se emite el evento para reiniciar el component schedule
+    @Output() recargarPagina = new EventEmitter();
+
+    public ArrayEmpleados: Empleado[];
+    public asd: any;
+    public fecha: any;
+
     constructor(
         private horarioService: HorarioService,
+        private empleadoService: AllempleadosService,
+        private datePipe: DatePipe
     ) {}
     
     Cerrar(): void{
@@ -19,14 +36,18 @@ export class AgregarActualizacionComponent implements OnInit {
     }
 
     ngOnInit(): void {
-
+        this.empleadoService.MostrarEmpleados().subscribe(
+            response => {
+                this.ArrayEmpleados = response.data;
+                this.fecha = this.datePipe.transform(new Date(), 'dd - mm - yyyy');
+            },
+            error => {
+                console.error(error)
+            }
+        )
     }
 
     enviarActualizacion(): void{
 
-    }
-
-    cargarData(): void {
-        
     }
 }
